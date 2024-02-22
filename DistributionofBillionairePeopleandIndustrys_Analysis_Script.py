d10391a39d9b629be0c402573062fa159d89c8f2
Angela272 <natescobar2@gmail.com>
2024-02-22 16:57:38 -0500

    Update script and readme

---
+++ DistributionofBillionairePeopleandIndustrys_Analysis_Script.py
@@ -0,0 +1,208 @@
+import streamlit as st
+import pandas as pd
+import folium
+import plotly.express as px
+from streamlit_folium import st_folium
+from streamlit_folium import folium_static
+import branca.colormap as cm
+import geopandas as gpd
+from folium import plugins
+
+
+#10 countrys with more billionaires
+#10 countrys with more money
+#10 industrys with more billionaires
+#10 industrys with more money
+#pie chart industrys
+#pie chart countrys
+#mapa del mundo heatmap de acuerdo al conteo de billonarios.
+
+
+st.set_page_config(page_title="Distribution_Billionaires - Analysis",
+                   page_icon="bar_chart:",
+                   layout="wide")
+
+
+# Read the CSV file into a DataFrame
+richest_people = pd.read_csv("500_richest_people_2021_clean.csv")
+
+# Create a new DataFrame with the count of each value in the 'Country' column
+country_counts = richest_people['Country'].value_counts().reset_index()
+country_counts.columns = ['Country', 'Count']
+
+# Create the country_money DataFrame
+country_money = richest_people.groupby('Country')['Total_Networth_Billions'].sum().reset_index()
+country_money.columns = ['Country', 'Total_Networth_Billions']
+# Sort the DataFrame in descending order by 'Total_Networth_Billions'
+country_money = country_money.sort_values(by='Total_Networth_Billions', ascending=False)
+# Reset the index and add a column numbered from 1
+country_money.reset_index(drop=True, inplace=True)
+country_money.index += 1
+# Show the country_money DataFrame
+print(country_money)
+
+#______________ Side bar _________
+st.sidebar.title("Distribution of Billionaire People and Industrys")
+st.sidebar.header("Please filter here:")
+category = st.sidebar.selectbox(
+    "Category",
+    [" ", "Countrys", "Industrys"]
+)
+
+feature = st.sidebar.selectbox(
+    "Feature",
+    [" ", "With more Billionaires", "With more money"]
+)
+
+
+if category == "Countrys" and feature == "With more Billionaires":
+    st.header("Countrys with more Billionaires")
+
+    ###MAP COUNTRYS WITH MORE BILLIONAIRES
+    # Create a map centered at a specific location with zoom level 1.45
+    map = folium.Map(location=[30, -20], zoom_start=1.45)
+
+    # Add a choropleth layer with custom style and tooltip
+    folium.Choropleth(
+        geo_data="countries.geojson",
+        name="choropleth",
+        data=country_counts,
+        columns=["Country", "Count"],
+        key_on="feature.properties.ADMIN",
+        fill_color="YlOrRd",  # Yellow to Red color palette
+        fill_opacity=0.7,
+        line_opacity=0.2,
+        nan_fill_color="gray",
+        nan_fill_opacity=0.4,
+        legend_name="Count",
+        highlight=True,
+        threshold_scale=country_counts['Count'].quantile([i/20 for i in range(21)]).tolist(),  # Define 20 quantiles
+        tooltip=folium.features.GeoJsonTooltip(fields=['ADMIN', 'Count'], aliases=['Country', 'Count'], labels=True, sticky=False)
+    ).add_to(map)
+
+    # Display the map
+    folium_static(map, width=600, height=400)
+
+    ##BAR CHART COUNTRYS WITH MORE BILLIONAIRES
+    # Sort the DataFrame by 'Count' in descending order and get the top 15 countries
+    top_countries = country_counts.sort_values('Count', ascending=False).head(15)
+
+
+    # Create an interactive line chart with points using Plotly
+
+    # Create a bar chart
+    fig = px.bar(top_countries, x='Country', y='Count', title='Top 15 Countries with more billionaires')
+
+    # Update the layout to change the y-axis label
+    fig.update_layout(yaxis_title='Number of Billionaires')
+
+    # Display the chart
+    st.plotly_chart(fig)
+
+
+    country_counts
+
+if category == "Countrys" and feature == "With more money":
+    st.header("Countrys with more money from Billionaires")
+
+    ###MAP COUNTRYS WITH MORE MONEY 
+    # Create a map centered at a specific location with zoom level 1.45
+    map = folium.Map(location=[30, -20], zoom_start=1.45)
+
+    # Add a choropleth layer with custom style and tooltip
+    folium.Choropleth(
+        geo_data="countries.geojson",
+        name="choropleth",
+        data=country_money,
+        columns=["Country", "Total_Networth_Billions"],
+        key_on="feature.properties.ADMIN",
+        fill_color="YlOrRd", 
+        fill_opacity=0.7,
+        line_opacity=0.2,
+        nan_fill_color="gray",
+        nan_fill_opacity=0.4,
+        legend_name="Total Net Worth (Billions)",
+        highlight=True,
+        threshold_scale=country_money['Total_Networth_Billions'].quantile([i/50 for i in range(51)]).tolist(),  
+        tooltip=folium.features.GeoJsonTooltip(fields=['ADMIN', 'Total_Networth_Billions'], aliases=['Country', 'Total_Networth_Billions'], labels=True, sticky=False)
+    ).add_to(map)
+    
+    # Display the map
+    folium_static(map, width=600, height=400)
+
+    ###Bar Chart 
+
+    # Filter the top 15 countries
+    top_15_countries = country_money.head(15)
+
+    # Create a bar chart for the top 15 countries
+    fig = px.bar(top_15_countries, x='Country', y='Total_Networth_Billions',
+                title='Top 15 Countries by Total Net Worth from Billionaires',
+                labels={'Total_Networth_Billions': 'Total Net Worth (Billions)', 'Country': 'Country'},
+                )
+
+    # Update the layout to customize the chart appearance
+    fig.update_layout(xaxis_title='Country', yaxis_title='Total Net Worth (Billions)',
+                    yaxis_tickformat='$.2f',  # Format y-axis as currency with 2 decimal places
+                    )
+
+    # Display the chart
+    st.plotly_chart(fig)
+
+    country_money
+
+if category == "Industrys" and feature == "With more Billionaires":
+
+    # Count the number of occurrences of each value in the "Industry" column
+    industry_counts = richest_people['Industry'].value_counts()
+
+    # Create a bar chart
+    fig = px.bar(x=industry_counts.index, y=industry_counts.values,
+                labels={'y': 'Number of Billionaires', 'x': 'Industry'},
+                title='Number of Billionaires by Industry')
+
+    # Display the chart in your Streamlit app
+    st.plotly_chart(fig)
+
+    # Create a donut chart
+    fig = px.pie(names=industry_counts.index, values=industry_counts.values,
+                title='Percentage of Billionaires by Industry',
+                hole=0.5)  # Set the size of the hole in the middle to create a donut chart
+
+    # Display the chart
+    st.plotly_chart(fig)
+
+
+
+if category == "Industrys" and feature == "With more money":
+
+    # Sum the "Total_Networth_Billions" column grouped by "Industry"
+    industry_total_networth = richest_people.groupby('Industry')['Total_Networth_Billions'].sum().reset_index()
+
+    # Create a bar chart
+    fig = px.bar(industry_total_networth, x='Industry', y='Total_Networth_Billions',
+                title='Total Net Worth by Industry',
+                labels={'Total_Networth_Billions': 'Total Net Worth (Billions)', 'Industry': 'Industry'},
+                )
+
+    # Update the layout to customize the chart appearance
+    fig.update_layout(xaxis_title='Industry', yaxis_title='Total Net Worth (Billions)',
+                    yaxis_tickformat='$.2f',  # Format y-axis as currency with 2 decimal places
+                    )
+
+    # Display the chart in your Streamlit app
+    st.plotly_chart(fig)
+
+
+
+
+    # Sum the "Total_Networth_Billions" column grouped by "Industry"
+    industry_total_networth = richest_people.groupby('Industry')['Total_Networth_Billions'].sum().reset_index()
+
+    # Create a donut chart
+    fig = px.pie(industry_total_networth, names='Industry', values='Total_Networth_Billions',
+                title='Total Net Worth by Industry',
+                hole=0.5)  # Set the size of the hole in the middle to create a donut chart
+
+    # Display the chart in your Streamlit app
+    st.plotly_chart(fig)
--- README.md
+++ README.md
@@ -1,1 +1,90 @@
-# 500_Richest_People
+### **Distribution of Billionaire People and Industrys**
+####  By Angela Escobar
+
+
+
+# Table of Contents
+1. [Access to the Streamlit app](#access-to-the-streamlit-app)
+2. [Project description and objective](#project-description-and-objective)
+3. [Data preparation and processing](#data-preparation-and-processing)
+4. [Analysis and development of the Streamlit app](#analysis-and-development-of-the-streamlit-app)
+5. [Conclusions](#conclusions)
+6. [Tech Stack](#tech-stack)
+
+
+
+
+## Access to the streamlit app: 
+You can access to the interactive dashboard at the following link: 
+
+
+## Project description and objective
+
+
+This project analyzed a database containing information about the 500 wealthiest people in the world, including their wealth, location, and industry. The purpose was to identify patterns and trends in global wealth distribution, as well as to better understand the dynamics of the global economy and the impact of individual fortunes on different sectors and industries.
+
+The project aimed to answer the following specific questions:
+
+Which countries and industries have the highest number of billionaires and capital movement?
+
+## Data preparation and processing
+
+In the preparation of this analysis project, the necessary metrics were determined to conduct the study. The database was located, downloaded, and subsequently securely and systematically organized on the computer. Additionally, se creo el repositorio para tener control de versiones disponible to ensure the security of the information.
+
+For data processing, a thorough cleaning procedure was conducted, focusing on identifying erroneous or inaccurately entered data and removing any duplicated information. Additionally, outliers were identified and handled appropriately to ensure the integrity and accuracy of the dataset. Data normalization and transformation techniques were applied as needed to prepare the data for analysis. 
+
+This is a list of the specific actions that were carried out. More detailed information can be found in the project code.
+
+* Import datasets specifying separator
+* Remove rows with missing values 
+* Clean column names
+* Remove empty spaces
+* Review and correct the data type of each column
+* Hide columns that would not be relevant for the analysis.
+* Filter some columns to omit zero values
+* Rename columns
+
+
+## Analysis and development of the Streamlit app
+
+An interactive dashboard was developed to facilitate the user's handling and understanding of the information. The user interface appeared as follows:
+
+![Texto alternativo](Visualizations/9.jpg)
+
+The specific aspects targeted for analysis were initially outlined, involving an intricate breakdown of the key focal areas slated for examination. The detailed execution of the analysis are further elaborated within the project's codebase. These are the results obtained after the process of analysis.
+
+![Texto alternativo](Visualizations/1.jpg)
+![Texto alternativo](Visualizations/2.jpg)
+![Texto alternativo](Visualizations/3.jpg)
+![Texto alternativo](Visualizations/4.jpg)
+![Texto alternativo](Visualizations/5.jpg)
+![Texto alternativo](Visualizations/6.jpg)
+![Texto alternativo](Visualizations/7.jpg)
+![Texto alternativo](Visualizations/8.jpg)
+
+
+
+## Conclusions
+
+* The United States is the country with the highest number of billionaires, totaling 157, more than double that of China, which is the next country with 77 billionaires, followed by Germany and Russia.
+
+* Despite France being in 7th place in the ranking by number of billionaires, in the ranking by total net worth of billionaires, it is in 3rd place, after China and the US respectively. This indicates that in France, although there are fewer people, their fortune is greater than that of billionaires from countries such as Germany, Russia, Hong Kong, India, and the United Kingdom.
+
+* If all billionaires from Germany, Russia, Hong Kong, India, the United Kingdom, France, Canada, and Sweden were to combine their fortunes, it would still be less than that of billionaires from the United States.
+
+* The most profitable industry is the technology sector, accounting for 15% of this ranking, followed by the industrial sector and diversified investments.
+
+* The technology industry in 2021 is the main source of income for a significant percentage of billionaires across the world, accumulating a total net worth of over 2000 billion dollars.
+
+
+
+## Tech Stack
+
+  Python
+
+  Streamlit
+
+  Visual Studio
+
+
+_______________END OF CASE STUDY______________________
